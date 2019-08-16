7edfc5de7b2c332a74ea65be66fbb84a4df93562
Badr Abdullah <babdullah@ws63lx.lsv.uni-saarland.de>
2019-08-16 18:03:45 +0200

    add CBOW directory

---
+++ CBOW_embeddings/vectorization.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+
+from argparse import Namespace
+from collections import Counter
+import json
+import os
+import string
+
+import numpy as np
+import pandas as pd
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+
+# Data Vectorization classes
+# The Vocabulary
+
+class Vocabulary(object):
+    """Class to process text and extract vocabulary for mapping"""
+
+    def __init__(self, token_to_idx=None, mask_token="<MASK>", add_unk=True, unk_token="<UNK>"):
+        """
+        Args:
+            token_to_idx (dict): a pre-existing map of tokens to indices
+            mask_token (str): the MASK token to add into the Vocabulary; indicates
+                a position that will not be used in updating the model's parameters
+            add_unk (bool): a flag that indicates whether to add the UNK token
+            unk_token (str): the UNK token to add into the Vocabulary
+
+        """
+
+        if token_to_idx is None:
+            token_to_idx = {}
+        self._token_to_idx = token_to_idx
+
+        self._idx_to_token = {idx: token
+                              for token, idx in self._token_to_idx.items()}
+
+        self._add_unk = add_unk
+        self._unk_token = unk_token
+        self._mask_token = mask_token
+
+        self.mask_index = self.add_token(self._mask_token)
+        self.unk_index = -1
+        if add_unk:
+            self.unk_index = self.add_token(unk_token)
+
+    def to_serializable(self):
+        """ returns a dictionary that can be serialized """
+        return {'token_to_idx': self._token_to_idx,
+                'add_unk': self._add_unk,
+                'unk_token': self._unk_token,
+                'mask_token': self._mask_token}
+
+    @classmethod
+    def from_serializable(cls, contents):
+        """ instantiates the Vocabulary from a serialized dictionary """
+        return cls(**contents)
+
+    def add_token(self, token):
+        """Update mapping dicts based on the token.
+
+        Args:
+            token (str): the item to add into the Vocabulary
+        Returns:
+            index (int): the integer corresponding to the token
+        """
+        if token in self._token_to_idx:
+            index = self._token_to_idx[token]
+        else:
+            index = len(self._token_to_idx)
+            self._token_to_idx[token] = index
+            self._idx_to_token[index] = token
+        return index
+
+    def add_many(self, tokens):
+        """Add a list of tokens into the Vocabulary
+
+        Args:
+            tokens (list): a list of string tokens
+        Returns:
+            indices (list): a list of indices corresponding to the tokens
+        """
+        return [self.add_token(token) for token in tokens]
+
+    def lookup_token(self, token):
+        """Retrieve the index associated with the token
+          or the UNK index if token isn't present.
+
+        Args:
+            token (str): the token to look up
+        Returns:
+            index (int): the index corresponding to the token
+        Notes:
+            `unk_index` needs to be >=0 (having been added into the Vocabulary)
+              for the UNK functionality
+        """
+        if self.unk_index >= 0:
+            return self._token_to_idx.get(token, self.unk_index)
+        else:
+            return self._token_to_idx[token]
+
+    def lookup_index(self, index):
+        """Return the token associated with the index
+
+        Args:
+            index (int): the index to look up
+        Returns:
+            token (str): the token corresponding to the index
+        Raises:
+            KeyError: if the index is not in the Vocabulary
+        """
+        if index not in self._idx_to_token:
+            raise KeyError("the index (%d) is not in the Vocabulary" % index)
+        return self._idx_to_token[index]
+
+    def __str__(self):
+        return "<Vocabulary(size=%d)>" % len(self)
+
+    def __len__(self):
+        return len(self._token_to_idx)
+
+
+
+class CBOWVectorizer(object):
+    """ The Vectorizer which coordinates the Vocabularies and puts them to use"""
+    def __init__(self, cbow_vocab):
+        """
+        Args:
+            cbow_vocab (Vocabulary): maps words to integers
+        """
+        self.cbow_vocab = cbow_vocab
+
+    def vectorize(self, context, vector_length=-1):
+        """
+        Args:
+            context (str): the string of words separated by a space
+            vector_length (int): an argument for forcing the length of index vector
+        """
+
+        indices = [self.cbow_vocab.lookup_token(token) for token in context.split(' ')]
+        if vector_length < 0:
+            vector_length = len(indices)
+
+        out_vector = np.zeros(vector_length, dtype=np.int64)
+        out_vector[:len(indices)] = indices
+        out_vector[len(indices):] = self.cbow_vocab.mask_index
+
+        return out_vector
+
+    @classmethod
+    def from_dataframe(cls, cbow_df):
+        """Instantiate the vectorizer from the dataset dataframe
+
+        Args:
+            cbow_df (pandas.DataFrame): the target dataset
+        Returns:
+            an instance of the CBOWVectorizer
+        """
+        cbow_vocab = Vocabulary()
+        for index, row in cbow_df.iterrows():
+            for token in row.context.split(' '):
+                cbow_vocab.add_token(token)
+            cbow_vocab.add_token(row.target)
+
+        return cls(cbow_vocab)
+
+    @classmethod
+    def from_serializable(cls, contents):
+        cbow_vocab = \
+            Vocabulary.from_serializable(contents['cbow_vocab'])
+        return cls(cbow_vocab=cbow_vocab)
+
+    def to_serializable(self):
+        return {'cbow_vocab': self.cbow_vocab.to_serializable()}
+
+
+# The Dataset
+class CBOWDataset(Dataset):
+    def __init__(self, cbow_df, vectorizer):
+        """
+        Args:
+            cbow_df (pandas.DataFrame): the dataset
+            vectorizer (CBOWVectorizer): vectorizer instatiated from dataset
+        """
+        self.cbow_df = cbow_df
+        self._vectorizer = vectorizer
+
+        measure_len = lambda context: len(context.split(" "))
+        self._max_seq_length = max(map(measure_len, cbow_df.context))
+
+        self.train_df = self.cbow_df[self.cbow_df.split=='train']
+        self.train_size = len(self.train_df)
+
+        self.val_df = self.cbow_df[self.cbow_df.split=='val']
+        self.validation_size = len(self.val_df)
+
+        self.test_df = self.cbow_df[self.cbow_df.split=='test']
+        self.test_size = len(self.test_df)
+
+        self._lookup_dict = {'train': (self.train_df, self.train_size),
+                             'val': (self.val_df, self.validation_size),
+                             'test': (self.test_df, self.test_size)}
+
+        self.set_split('train')
+
+    @classmethod
+    def load_dataset_and_make_vectorizer(cls, cbow_csv):
+        """Load dataset and make a new vectorizer from scratch
+
+        Args:
+            cbow_csv (str): location of the dataset
+        Returns:
+            an instance of CBOWDataset
+        """
+        cbow_df = pd.read_csv(cbow_csv)
+        train_cbow_df = cbow_df[cbow_df.split=='train']
+        return cls(cbow_df, CBOWVectorizer.from_dataframe(train_cbow_df))
+
+    @classmethod
+    def load_dataset_and_load_vectorizer(cls, cbow_csv, vectorizer_filepath):
+        """Load dataset and the corresponding vectorizer.
+        Used in the case in the vectorizer has been cached for re-use
+
+        Args:
+            cbow_csv (str): location of the dataset
+            vectorizer_filepath (str): location of the saved vectorizer
+        Returns:
+            an instance of CBOWDataset
+        """
+        cbow_df = pd.read_csv(cbow_csv)
+        vectorizer = cls.load_vectorizer_only(vectorizer_filepath)
+        return cls(cbow_df, vectorizer)
+
+    @staticmethod
+    def load_vectorizer_only(vectorizer_filepath):
+        """a static method for loading the vectorizer from file
+
+        Args:
+            vectorizer_filepath (str): the location of the serialized vectorizer
+        Returns:
+            an instance of CBOWVectorizer
+        """
+        with open(vectorizer_filepath) as fp:
+            return CBOWVectorizer.from_serializable(json.load(fp))
+
+    def save_vectorizer(self, vectorizer_filepath):
+        """saves the vectorizer to disk using json
+
+        Args:
+            vectorizer_filepath (str): the location to save the vectorizer
+        """
+        with open(vectorizer_filepath, "w") as fp:
+            json.dump(self._vectorizer.to_serializable(), fp)
+
+    def get_vectorizer(self):
+        """ returns the vectorizer """
+        return self._vectorizer
+
+    def set_split(self, split="train"):
+        """ selects the splits in the dataset using a column in the dataframe """
+        self._target_split = split
+        self._target_df, self._target_size = self._lookup_dict[split]
+
+    def __len__(self):
+        return self._target_size
+
+    def __getitem__(self, index):
+        """the primary entry point method for PyTorch datasets
+
+        Args:
+            index (int): the index to the data point
+        Returns:
+            a dictionary holding the data point's features (x_data) and label (y_target)
+        """
+        row = self._target_df.iloc[index]
+
+        context_vector = \
+            self._vectorizer.vectorize(row.context, self._max_seq_length)
+        target_index = self._vectorizer.cbow_vocab.lookup_token(row.target)
+
+        return {'x_data': context_vector,
+                'y_target': target_index}
+
+    def get_num_batches(self, batch_size):
+        """Given a batch size, return the number of batches in the dataset
+
+        Args:
+            batch_size (int)
+        Returns:
+            number of batches in the dataset
+        """
+        return len(self) // batch_size
+
+def generate_batches(dataset, batch_size, shuffle=True,
+                     drop_last=True, device="cpu"):
+    """
+    A generator function which wraps the PyTorch DataLoader. It will
+      ensure each tensor is on the write device location.
+    """
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
+                            shuffle=shuffle, drop_last=drop_last)
+
+    for data_dict in dataloader:
+        out_data_dict = {}
+        for name, tensor in data_dict.items():
+            out_data_dict[name] = data_dict[name].to(device)
+        yield out_data_dict
