1142dc8b531a6696844bad98b29058dbcdd7e74a
Badr Abdullah <babdullah@ws63lx.lsv.uni-saarland.de>
2019-08-19 15:13:25 +0200

    add convo text classification model

---
+++ CNN_news_classifictation/nn_train.py
@@ -0,0 +1,348 @@
+# -*- coding: utf-8 -*-
+
+from argparse import Namespace
+from collections import Counter
+import json
+import os
+import re
+import string
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+# own imports
+import nn_models
+import vectorization
+
+# Training Routine
+# Helper functions
+def make_train_state(args):
+    return {'stop_early': False,
+            'early_stopping_step': 0,
+            'early_stopping_best_val': 1e8,
+            'learning_rate': args.learning_rate,
+            'epoch_index': 0,
+            'train_loss': [],
+            'train_acc': [],
+            'val_loss': [],
+            'val_acc': [],
+            'test_loss': -1,
+            'test_acc': -1,
+            'model_filename': args.model_state_file}
+
+
+def update_train_state(args, model, train_state):
+    """Handle the training state updates.
+
+    Components:
+     - Early Stopping: Prevent overfitting.
+     - Model Checkpoint: Model is saved if the model is better
+
+    :param args: main arguments
+    :param model: model to train
+    :param train_state: a dictionary representing the training state values
+    :returns:
+        a new train_state
+    """
+
+    # Save one model at least
+    if train_state['epoch_index'] == 0:
+        torch.save(model.state_dict(), train_state['model_filename'])
+        train_state['stop_early'] = False
+
+    # Save model if performance improved
+    elif train_state['epoch_index'] >= 1:
+        loss_tm1, loss_t = train_state['val_loss'][-2:]
+
+        # If loss worsened
+        if loss_t >= train_state['early_stopping_best_val']:
+            # Update step
+            train_state['early_stopping_step'] += 1
+        # Loss decreased
+        else:
+            # Save the best model
+            if loss_t < train_state['early_stopping_best_val']:
+                torch.save(model.state_dict(), train_state['model_filename'])
+
+            # Reset early stopping step
+            train_state['early_stopping_step'] = 0
+
+        # Stop early ?
+        train_state['stop_early'] = \
+            train_state['early_stopping_step'] >= args.early_stopping_criteria
+
+    return train_state
+
+def compute_accuracy(y_pred, y_target):
+    _, y_pred_indices = y_pred.max(dim=1)
+    n_correct = torch.eq(y_pred_indices, y_target).sum().item()
+    return n_correct / len(y_pred_indices) * 100
+
+
+
+def set_seed_everywhere(seed, cuda):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed_all(seed)
+
+def handle_dirs(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+def load_glove_from_file(glove_filepath):
+    """
+    Load the GloVe embeddings
+
+    Args:
+        glove_filepath (str): path to the glove embeddings file
+    Returns:
+        word_to_index (dict), embeddings (numpy.ndarary)
+    """
+
+    word_to_index = {}
+    embeddings = []
+    with open(glove_filepath, "r", encoding='utf-8') as fp:
+        for index, line in enumerate(fp):
+            line = line.split(" ") # each line: word num1 num2 ...
+            word_to_index[line[0]] = index # word = line[0]
+            embedding_i = np.array([float(val) for val in line[1:]])
+            embeddings.append(embedding_i)
+    return word_to_index, np.stack(embeddings)
+
+
+def make_embedding_matrix(glove_filepath, words):
+    """
+    Create embedding matrix for a specific set of words.
+
+    Args:
+        glove_filepath (str): file path to the glove embeddigns
+        words (list): list of words in the dataset
+    """
+    word_to_idx, glove_embeddings = load_glove_from_file(glove_filepath)
+    embedding_size = glove_embeddings.shape[1]
+
+    final_embeddings = np.zeros((len(words), embedding_size))
+
+    for i, word in enumerate(words):
+        if word in word_to_idx:
+            final_embeddings[i, :] = glove_embeddings[word_to_idx[word]]
+        else:
+            embedding_i = torch.ones(1, embedding_size)
+            torch.nn.init.xavier_uniform_(embedding_i)
+            final_embeddings[i, :] = embedding_i
+
+    return final_embeddings
+
+
+args = Namespace(
+    # Data and Path hyper parameters
+    news_csv="../data/news_documents/news_dataset.csv",
+    vectorizer_file="vectorizer.json",
+    model_state_file="model.pth",
+    save_dir="model_storage/cnn_doc_classification",
+    # Model hyper parameters
+    glove_filepath='../data/glove.6B/glove.6B.100d.txt',
+    use_glove=False,
+    embedding_size=100,
+    hidden_dim=100,
+    num_channels=100,
+    # Training hyper parameter
+    seed=1337,
+    learning_rate=0.001,
+    dropout_p=0.1,
+    batch_size=124,
+    num_epochs=100,
+    early_stopping_criteria=5,
+    # Runtime option
+    cuda=True,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepaths_to_save_dir=True
+)
+
+if args.expand_filepaths_to_save_dir:
+    args.vectorizer_file = os.path.join(args.save_dir,
+                                        args.vectorizer_file)
+
+    args.model_state_file = os.path.join(args.save_dir,
+                                         args.model_state_file)
+
+    print("Expanded filepaths: ")
+    print("\t{}".format(args.vectorizer_file))
+    print("\t{}".format(args.model_state_file))
+
+# Check CUDA
+if not torch.cuda.is_available():
+    args.cuda = False
+
+args.device = torch.device("cuda" if args.cuda else "cpu")
+print("Using CUDA: {}".format(args.cuda))
+
+# Set seed for reproducibility
+set_seed_everywhere(args.seed, args.cuda)
+
+# handle dirs
+handle_dirs(args.save_dir)
+
+
+# Initializations
+#args.use_glove = True
+
+if args.reload_from_files:
+    # training from a checkpoint
+    dataset = vectorization.NewsDataset.load_dataset_and_load_vectorizer(args.news_csv,
+                                                              args.vectorizer_file)
+else:
+    # create dataset and vectorizer
+    dataset = vectorization.NewsDataset.load_dataset_and_make_vectorizer(args.news_csv)
+    dataset.save_vectorizer(args.vectorizer_file)
+
+vectorizer = dataset.get_vectorizer()
+
+# Use GloVe or randomly initialized embeddings
+print('Reading embeddings from disk ...')
+if args.use_glove:
+    words = vectorizer.title_vocab._token_to_idx.keys()
+    embeddings = make_embedding_matrix(glove_filepath=args.glove_filepath,
+                                       words=words)
+    print("Using pre-trained embeddings")
+else:
+    print("Not using pre-trained embeddings")
+    embeddings = None
+
+classifier = nn_models.NewsClassifier(embedding_size=args.embedding_size,
+                            num_embeddings=len(vectorizer.title_vocab),
+                            num_channels=args.num_channels,
+                            hidden_dim=args.hidden_dim,
+                            num_classes=len(vectorizer.category_vocab),
+                            dropout_p=args.dropout_p,
+                            pretrained_embeddings=embeddings,
+                            padding_idx=0)
+
+# Training loop
+classifier = classifier.to(args.device)
+dataset.class_weights = dataset.class_weights.to(args.device)
+
+loss_func = nn.CrossEntropyLoss(dataset.class_weights)
+optimizer = optim.Adam(classifier.parameters(), lr=args.learning_rate)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
+                                           mode='min', factor=0.5,
+                                           patience=1)
+
+train_state = make_train_state(args)
+
+epoch_bar = tqdm(desc='training routine',
+                          total=args.num_epochs,
+                          position=0)
+
+dataset.set_split('train')
+train_bar = tqdm(desc='split=train',
+                          total=dataset.get_num_batches(args.batch_size),
+                          position=1,
+                          leave=True)
+dataset.set_split('val')
+val_bar = tqdm(desc='split=val',
+                        total=dataset.get_num_batches(args.batch_size),
+                        position=1,
+                        leave=True)
+
+try:
+    for epoch_index in range(args.num_epochs):
+        train_state['epoch_index'] = epoch_index
+
+        # Iterate over training dataset
+
+        # setup: batch generator, set loss and acc to 0, set train mode on
+
+        dataset.set_split('train')
+        batch_generator = vectorization.generate_batches(dataset,
+                                           batch_size=args.batch_size,
+                                           device=args.device)
+        running_loss = 0.0
+        running_acc = 0.0
+        classifier.train()
+
+        for batch_index, batch_dict in enumerate(batch_generator):
+            # the training routine is these 5 steps:
+
+            # --------------------------------------
+            # step 1. zero the gradients
+            optimizer.zero_grad()
+
+            # step 2. compute the output
+            y_pred = classifier(batch_dict['x_data'])
+
+            # step 3. compute the loss
+            loss = loss_func(y_pred, batch_dict['y_target'])
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (batch_index + 1)
+
+            # step 4. use loss to produce gradients
+            loss.backward()
+
+            # step 5. use optimizer to take gradient step
+            optimizer.step()
+            # -----------------------------------------
+            # compute the accuracy
+            acc_t = compute_accuracy(y_pred, batch_dict['y_target'])
+            running_acc += (acc_t - running_acc) / (batch_index + 1)
+
+            # update bar
+            train_bar.set_postfix(loss=running_loss, acc=running_acc,
+                                  epoch=epoch_index)
+            train_bar.update()
+
+        train_state['train_loss'].append(running_loss)
+        train_state['train_acc'].append(running_acc)
+
+        # Iterate over val dataset
+
+        # setup: batch generator, set loss and acc to 0; set eval mode on
+        dataset.set_split('val')
+        batch_generator = vectorization.generate_batches(dataset,
+                                           batch_size=args.batch_size,
+                                           device=args.device)
+        running_loss = 0.
+        running_acc = 0.
+        classifier.eval()
+
+        for batch_index, batch_dict in enumerate(batch_generator):
+
+            # compute the output
+            y_pred =  classifier(batch_dict['x_data'])
+
+            # step 3. compute the loss
+            loss = loss_func(y_pred, batch_dict['y_target'])
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (batch_index + 1)
+
+            # compute the accuracy
+            acc_t = compute_accuracy(y_pred, batch_dict['y_target'])
+            running_acc += (acc_t - running_acc) / (batch_index + 1)
+            val_bar.set_postfix(loss=running_loss, acc=running_acc,
+                            epoch=epoch_index)
+            val_bar.update()
+
+        train_state['val_loss'].append(running_loss)
+        train_state['val_acc'].append(running_acc)
+
+        train_state = update_train_state(args=args, model=classifier,
+                                         train_state=train_state)
+
+        scheduler.step(train_state['val_loss'][-1])
+
+        if train_state['stop_early']:
+            break
+
+        train_bar.n = 0
+        val_bar.n = 0
+        epoch_bar.update()
+
+except KeyboardInterrupt:
+    print("Exiting loop")
